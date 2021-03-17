d43ac3fe62ddafcd2aff8da5816086c15b1c0701
Park SeJin <sjsj9368@gmail.com>
2021-03-17 15:48:22 +0900

    2021.03.17 | add algorithm

---
+++ ALGORITHM/SOURCE/03. Stack & Quequ(스택과 큐)/10. 회전하는 큐.py
@@ -0,0 +1,32 @@
+import sys
+input = sys.stdin.readline
+from collections import deque
+
+n, m = map(int, input().split())  # n:큐의크기, m:뽑아내려는수
+want = list(map(int, input().split()))
+queue = deque(i for i in range(1, n + 1))
+
+
+def min_count(x, y):
+    count = 0
+    if x.index(y) <= len(x) - x.index(y):
+        count = x.index(y)
+        for _ in range(x.index(y)):
+            x.append(x.popleft())
+    else:
+        count = len(x) - x.index(y)
+        for _ in range(len(x) - x.index(y)):
+            x.appendleft(x.pop())
+    return x, count
+
+
+answer = 0
+for i in want:
+    queue, count = min_count(queue, i)
+    answer += count
+
+    if queue[0] == i:
+        queue.popleft()
+        pass
+
+print(answer)
